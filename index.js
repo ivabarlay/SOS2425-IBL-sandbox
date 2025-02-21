1fd42c9e0f9367586e4a77de761bc380a8aaf2a8
ivabarlay <ivabarlay@proton.me>
2025-02-21 12:28:59 +0100

    PORT arreglado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cool = require("cool-ascii-faces");
-const PORT = 16078;
+const PORT = process.env.PORT || 16078;
 
 app.use("/", express.static("./public"));
 
